0ba1d2fbc459c8ae67c2cdc20f3b47fba2f34b16
macos <macos@macoss-iMac.local>
2020-07-06 14:12:11 +0200

    Two views with a navigation controller and an alert

--- KidsApplication/ViewController.swift
+++ KidsApplication/ViewController.swift
@@ -9,17 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var email: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func Loginbtn(_ sender: Any) {
+        if email.text == ("user@gmail.com") && password.text == ("user")
+        {
+            let sec: HomeViewController = self.storyboard?.instantiateViewController(withIdentifier: "home") as! HomeViewController;
+            self.navigationController?.pushViewController(sec, animated: true)
+        }
+        else{
+            let alert = UIAlertController(title:"Wrong credentials!", message: "Type correct email and password!", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title:"OK", style: .default, handler:nil))
+                
+                self.present(alert, animated: true,completion: nil)
+            
+        }
+    }
+    
 }
 
